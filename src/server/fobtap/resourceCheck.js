7ae20c1a3b6f41c0c3269abc671183b2c93ad9f9
trhode <trhode84@gmail.com>
2018-04-21 19:14:20 -0700

    1

--- src/server/fobtap/resourceCheck.js
+++ src/server/fobtap/resourceCheck.js
@@ -4,12 +4,12 @@ const validators = require('../spec/validators')
 const events = require('../events')
 
 module.exports = function(req, res, next){
-    
+
     let memberId = utils.memberIdFromFob(req.body.fob)
     let resource = utils.getResource(req.body.resourceId)
     console.log('resource check middleware', {memberId, resource})
     if (memberId && resource){
-        events.resourceEvs.resourceUsed(
+        events.resourcesEvs.resourceUsed(
           req.body.resourceId,
           memberId,
           req.body.amount || 1,
