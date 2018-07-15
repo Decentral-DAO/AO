a06a51c63e674bc235c4090578b84245e48c7a40
Taylor Rhodes <taylorsingletonfookes@live.com>
2018-07-15 16:03:45 -0700

    node connection qr

--- src/components/Nodes/index.vue
+++ src/components/Nodes/index.vue
@@ -5,11 +5,13 @@
     p Lightning Node Status: Active
     p We have {{ activeChannels }} active channels.
     p We have {{ confirmedBalance }} satoshis.
+    p Connect to our node from a lightning wallet using this qr:
+    div(v-html='tag')
 
 </template>
 
 <script>
-
+import qrcode from 'qrcode-generator'
 import SharedTitle from '../slotUtils/SharedTitle'
 
 export default {
@@ -19,8 +21,23 @@ export default {
         },
         confirmedBalance(){
             return this.$store.state.nodes.lnd.wallet.confirmed_balance
-        }
-
+        },
+        tag(){
+            let data = this.$store.state.nodes.lnd.info.identity_pubkey + "@dctrl.ca:9735"
+            let typeNumber = 10;
+            let errorCorrectionLevel = 'L';
+            let qr = qrcode(typeNumber, errorCorrectionLevel);
+            try {
+                qr.addData(data)
+                qr.make()
+            } catch(err) {
+               return console.log('err from qrcode', err)
+            }
+            let cellsize = 4
+            let margin = 2
+            let tag = qr.createImgTag(cellsize, margin)
+            return tag
+        },
     },
     components:{
         SharedTitle
